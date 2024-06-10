c5c1766aac9e9f86a75a769af4b84213eb8e1015
arjacomuea <ar.jacomea@uea.edu.ec>
2024-06-09 23:20:42 -0500

    version2

--- PROGRAMA1.py
+++ PROGRAMA1.py
@@ -1,22 +1,29 @@
-#ABSTRACCION
-class Cajero:
-    def _init_(self, marca, tipo, trato):
-        self.marca = marca
-        self.tipo = tipo
-        self.trato = trato
-    #metod par mostrar
-    def detalles(self):
-        print("Cajero Marca:    ",self.marca,"tipo:", self.tipo, " Trato:",self.trato)
+#2. ENCAPSULAMIENTO
+class Saldo:
+    def _init_(self):
+        self.Saldo_valor = 1256 #encapuslo el valor 120
+        self.__valor = 120 #encapuslo el valor 120
+        self.__limite =5000 #encapuslo el valor maximo de retiro
+    #metodo get para traer el metodo para la variable encapusalda
 
-#instanciar obejtos de la calse cajero D
-#OBEJTO1 CAJERO TIPO PAGO
-cajeroPago=Cajero("Diebol", "Pago", "ISLA")
-#OBEJTO1 CAJERO TIPO RETIRO
-cajeroRetiro=Cajero("NFC", "Retiro", "AGENCIA")
-#OBEJTO1 CAJERO TIPO RECICLADOR
-cajeroReciclador=Cajero("diebol", "Reciclador", "isla")
+    def get_cuenta(self):  #solo un metodo encapsulado que improme mensaje
+        print("El valor de cuenta es:")
+    def get_valor(self):
+        return self.__valor
 
-##llamo al metodo detalles de cada cajero e imprmire
-cajeroPago.detalles()
-cajeroRetiro.detalles()
-cajeroReciclador.detalles()
+    #otro get para trader el metodo que obtenga la valrible encapusadla limiete
+    def get_limite(self):
+        return self.__limite
+
+#Instancio la clase  Saldo  al crear un objeto para que funicione
+CuentaSaldo = Saldo()
+#creo objeto para llamar al valor oculto
+#objeto.nombreClase_nombre atributo //llamo a los ocultos
+print(CuentaSaldo.get_cuenta()) #llamo al mensaje
+print(CuentaSaldo.get_valor()) #llamo el valor oculto del saldo
+print(CuentaSaldo.Saldo_valor)
+print("Cupo mAXIMO de retiro")
+#llamo el 2 valir protegido el limiete
+print(CuentaSaldo.get_limite())
+
+
